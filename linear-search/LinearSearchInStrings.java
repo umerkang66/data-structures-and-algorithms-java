bc567701f5757fc50a8a7c7d3f85d80755e53489
umerkang66 <umerkang66@gmail.com>
2021-09-17 22:09:38 +0500

    inital commit and completed linear search in strings and arrays

---
+++ linear-search/LinearSearchInStrings.java
@@ -0,0 +1,29 @@
+public class LinearSearchInStrings {
+  public boolean linearSearch(String string, char target) {
+    if (string.length() <= 0) {
+      return false;
+    }
+
+    for (int i = 0; i < string.length(); i++) {
+      if (target == string.charAt(i)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public boolean linearSearch2(String string, char target) {
+    if (string.length() <= 0) {
+      return false;
+    }
+
+    for (char ch : string.toCharArray()) {
+      if (target == ch) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ linear-search/MaxWealth.java
@@ -0,0 +1,18 @@
+public class MaxWealth {
+  public int maximumWealth(int[][] accounts) {
+    int maxAmount = 0;
+
+    for (int i = 0; i < accounts.length; i++) {
+      int sum = 0;
+      for (int j = 0; j < accounts[i].length; j++) {
+        sum += accounts[i][j];
+      }
+
+      if (sum > maxAmount) {
+        maxAmount = sum;
+      }
+    }
+
+    return maxAmount;
+  }
+}
