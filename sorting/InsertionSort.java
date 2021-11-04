44c1c587a14d1128d9ded4676fb57062830b2007
umerkang66 <umerkang66@gmail.com>
2021-11-04 12:04:57 +0500

    do question insertionSort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,28 +1,27 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-  public static void main(String[] args) {
-    int[] arr = { 5, 1, 1, 2, -23, -53, 0, 0 };
-    insertionSort(arr);
-
-    System.out.println(Arrays.toString(arr));
-  }
-
-  private static void insertionSort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = i + 1; j > 0; j--) {
-        if (arr[j] < arr[j - 1]) {
-          swap(arr, j, j - 1);
-        } else {
-          break;
-        }
-      }
+    public static void main(String[] args) {
+        int[] nums = { 5, 3, 4, 1, 2 };
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
     }
-  }
 
-  private static void swap(int[] arr, int first, int second) {
-    int temp = arr[first];
-    arr[first] = arr[second];
-    arr[second] = temp;
-  }
+    private static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (nums[j] < nums[j - 1]) {
+                    swap(nums, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
+    private static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
 }
---
+++ sorting/questions/FirstMissingPositive.java
@@ -0,0 +1,33 @@
+package questions;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = { 1 };
+        int missingPositive = missing(nums);
+        System.out.println(missingPositive);
+    }
+
+    private static int missing(int[] nums) {
+        int start = 0;
+        while (start < nums.length) {
+            int correctIndex = nums[start] - 1;
+            if (nums[start] > 0 && nums[start] <= nums.length && nums[start] != nums[correctIndex]) {
+                swap(nums, start, correctIndex);
+            } else {
+                start++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
