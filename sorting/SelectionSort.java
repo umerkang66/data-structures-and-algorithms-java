1071de0e80563e64445f9575d6def2a5f02eafcf
umerkang66 <umerkang66@gmail.com>
2021-11-04 12:13:26 +0500

    do selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,37 +1,35 @@
 import java.util.Arrays;
 
 public class SelectionSort {
-  public static void main(String[] args) {
-    int[] arr = { 5, 1, 1, 2, 0, 0 };
-    selectionSort(arr);
-
-    System.out.println(Arrays.toString(arr));
-  }
-
-  private static void selectionSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      // Find the maximum item in the array and swap with current (least maximum
-      // element of array) index
-      int last = arr.length - i - 1;
-      int maxIndex = getMaxIndex(arr, 0, last);
-
-      swap(arr, maxIndex, last);
-    }
-  }
-
-  private static int getMaxIndex(int[] arr, int start, int end) {
-    int max = start;
-    for (int i = start; i <= end; i++) {
-      if (arr[i] > arr[max])
-        max = i;
+    public static void main(String[] args) {
+        int[] arr = { 5, 1, 1, 2, 0, 0 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    return max;
-  }
+    private static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // Find the maximum item in the array and swap with current (least maximum
+            // element of array) index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
 
-  private static void swap(int[] arr, int first, int second) {
-    int temp = arr[first];
-    arr[first] = arr[second];
-    arr[second] = temp;
-  }
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
