ef9ac16f34ec142c5412b7bd4d72274ce2de111c
Umer Kang <umerkang66@gmail.com>
2021-12-05 08:51:01 +0500

    add recursive algorithm for binary search

--- recursion/RecursionBasics.java
+++ recursion/RecursionBasics.java
@@ -1,10 +1,14 @@
 public class RecursionBasics {
     public static void main(String[] args) {
-        int num = 7;
+        int num = 40;
         printNum(num);
 
         int fib = fibonacci(num);
         System.out.println(fib);
+
+        int[] nums = { 3, 9, 12, 15, 17, 22 };
+        int searchedResult = binarySearch(nums, 18, 0, nums.length - 1);
+        System.out.println(searchedResult);
     }
 
     private static void printNum(int num) {
@@ -13,6 +17,7 @@ public class RecursionBasics {
             return;
         }
         System.out.println(num);
+        // This is the last function call, that's why it is called TAIL RECURSION
         printNum(num + 1);
     }
 
@@ -25,5 +30,19 @@ public class RecursionBasics {
             return num;
         }
         return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+
+    private static int binarySearch(int[] nums, int target, int start, int end) {
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                return binarySearch(nums, target, start, mid - 1);
+            } else if (target > nums[mid]) {
+                return binarySearch(nums, target, mid + 1, end);
+            }
+        }
+        return -1;
     }
 }
