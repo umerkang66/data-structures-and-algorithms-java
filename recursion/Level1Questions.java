188095aa8909076f6f2b45f9661793dd6cf4b1aa
Umer Kang <umerkang66@gmail.com>
2021-12-23 17:59:42 +0500

    do 2 more recursion level 1 questions

--- recursion/Level1Questions.java
+++ recursion/Level1Questions.java
@@ -1,9 +1,18 @@
 public class Level1Questions {
+    private static int reverseActualSum = 0;
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println(factorial(n));
         printNto1(n);
         print1toN(n);
+        System.out.println(sumOfN(n));
+
+        System.out.println(sumOfDigits(125));
+        System.out.println(prodOfDigits(505));
+
+        reverseNum(1234);
+        System.out.println(reverseActualSum);
     }
 
     private static void printNto1(int n) {
@@ -23,10 +32,39 @@ public class Level1Questions {
     }
 
     private static int factorial(int n) {
-        // We donot have to go till 1 because 1 x "some-number" is always the "some-number" 
-        if (n == 2) {
+        if (n <= 1) {
             return n;
         }
-        return factorial(n - 1) * n;
+        return n * factorial(n - 1);
+    }
+
+    private static int sumOfN(int n) {
+        if (n <= 0) {
+            return n;
+        }
+        return n + sumOfN(n - 1);
+    }
+
+    private static int sumOfDigits(int num) {
+        if (num / 10 <= 0)
+            return num;
+        return num % 10 + sumOfDigits(num / 10);
+    }
+
+    private static int prodOfDigits(int num) {
+        if (num / 10 <= 0) {
+            return num;
+        }
+        return num % 10 * prodOfDigits(num / 10);
+    }
+
+    private static void reverseNum(int num) {
+        if (num == 0) {
+            return;
+        }
+        int remainder = num % 10;
+        // The "variable" below is is static class variable
+        reverseActualSum = reverseActualSum * 10 + remainder;
+        reverseNum(num / 10);
     }
 }
