3857ba5dffd8d174438ec00d405ec77bedf67d51
Umer Kang <umerkang66@gmail.com>
2021-12-28 17:14:11 +0500

    add arr rotation count again for practice

--- binary-search/src/com/umerkang/binarySearchInArrays/ArrRotationCount.java
+++ binary-search/src/com/umerkang/binarySearchInArrays/ArrRotationCount.java
@@ -2,14 +2,14 @@ package com.umerkang.binarySearchInArrays;
 
 public class ArrRotationCount {
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 0, 1, 2, 3};
-        int count = arrRotationCount(nums);
-
-        System.out.println(count);
+        int[] nums = {0, 1, 2, 4, 5, 6, 7};
+        int rotationCount = rotationCount(nums);
+        System.out.println(rotationCount);
     }
 
-    private static int arrRotationCount(int[] nums) {
+    private static int rotationCount(int[] nums) {
         int pivot = findPivot(nums);
+        // Array is rotated the pivot times + 1
         return pivot + 1;
     }
 
@@ -18,23 +18,25 @@ public class ArrRotationCount {
         int end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            // Case 1:
             if (mid < end && nums[mid] > nums[mid + 1]) {
+                // It is pivot because it is the largest number in the array
+                // In an ascending rotated sorted array every element will be smaller
+                // than the next element except the largest element in array
                 return mid;
             }
-            // Case 2:
             if (mid > start && nums[mid] < nums[mid - 1]) {
+                // If the mid is smaller than the previous element then the previous
+                // element is the largest element i.e. that is the pivot
+                // Because in normal ascending sorted array element will be always
+                // larger than the previous element
                 return mid - 1;
             }
-            // Case 3:
-            if (nums[mid] <= nums[start]) {
+            if (nums[mid] < nums[start]) {
                 end = mid - 1;
-            } // Case 4:
-            else if (nums[mid] > nums[start]) {
+            } else if (nums[start] <= nums[mid]) {
                 start = mid + 1;
             }
         }
-
         return -1;
     }
 }
