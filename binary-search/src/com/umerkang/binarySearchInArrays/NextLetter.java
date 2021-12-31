019ce723ea1e4eb843f9f3a801a448e5f0d6c958
Umer Kang <umerkang66@gmail.com>
2021-12-31 12:08:30 +0500

    add NextLetter question

---
+++ binary-search/src/com/umerkang/binarySearchInArrays/NextLetter.java
@@ -0,0 +1,31 @@
+package com.umerkang.binarySearchInArrays;
+
+public class NextLetter {
+    public static void main(String[] args) {
+        char[] characters = {'a', 'c', 'd', 'f', 'h' };
+        char target = 'f';
+        int result = nextLetter(characters, target);
+        System.out.println(result);
+    }
+
+    private static int nextLetter(char[] characters, char target) {
+        int start = 0;
+        int end = characters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < characters[mid]) {
+                end = mid - 1;
+            } else if (target > characters[mid]) {
+                start = mid + 1;
+            } else {
+                // If the element is present return the next element not the same element
+                start = mid + 1;
+            }
+        }
+        // If the element is not present then return the ceil
+        if (start < characters.length) {
+            return start;
+        }
+        return -1;
+    }
+}
