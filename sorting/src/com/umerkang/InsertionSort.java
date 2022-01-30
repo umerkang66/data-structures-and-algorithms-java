6c2bab272725df67d62274bf6f38a4713c4c0caa
Umer Kang <umerkang66@gmail.com>
2022-01-30 22:02:13 +0500

    add comments for explaining the insertion sort algorithm

--- sorting/src/com/umerkang/InsertionSort.java
+++ sorting/src/com/umerkang/InsertionSort.java
@@ -3,6 +3,16 @@ package com.umerkang;
 import java.util.Arrays;
 
 // For every index: put that element in the correct index of left-hand side
+// Time Complexity: Worst Case (descending sorted) O(n^2) and the best Case is linear
+// time complexity O(n)
+// Space complexity is constant, we are not using an extra object or array
+// WHY USE INSERTION SORT:
+// 1) It is adaptive, steps get reduced, if the array is sorted or nearly sorted, due
+// to breaking the loop
+// 2) It is stable
+// 3) Used for smaller values for N, works good when array is partially sorted, that's
+// why it takes part in hybrid sorting algorithms (hybrid algorithms' means it combines
+// merge sort or quick sort and combines some parts with insertion sort)
 public class InsertionSort {
     public static void main(String[] args) {
         int[] nums = {5, 3, 4, 1, 2};
@@ -15,6 +25,7 @@ public class InsertionSort {
         // is considered as i + 1, if "i" will point to the last element, "j" will
         // become index out of bounds
         for (int i = 0; i < nums.length - 1; i++) {
+            // "j" has to greater than zero to avoid index out of bounds
             for (int j = i + 1; j > 0; j--) {
                 // This loop will run from next element from i, and starts the loop to
                 // previous element till it reaches 0 + 1, because it checks for j - 1
