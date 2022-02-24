3dc234c271a66a4761420e6b0609012144128901
Umer Kang <umerkang66@gmail.com>
2022-02-24 20:34:41 +0500

    complete the recursion level 1 question video

---
+++ recursion-complete/src/com/umerkang/recursionEasy/Palindrome.java
@@ -0,0 +1,23 @@
+package com.umerkang.recursionEasy;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int n = 12321;
+        System.out.println(palindrome(n));
+    }
+
+    private static boolean palindrome(int n) {
+        int numLength = (int) (Math.log10(n) + 1);
+        return n == reverseNumber(n, numLength);
+    }
+
+    private static int reverseNumber(int n, int numLength) {
+        // Explanation is in reverseNumber question
+        if (n / 10 <= 0) {
+            return n;
+        }
+        int remainder = n % 10;
+        return remainder * (int) Math.pow(10, numLength - 1) + reverseNumber(n / 10,
+            numLength - 1);
+    }
+}
--- recursion-complete/src/com/umerkang/recursionEasy/ReverseNumber.java
+++ recursion-complete/src/com/umerkang/recursionEasy/ReverseNumber.java
@@ -4,15 +4,15 @@ public class ReverseNumber {
     public static void main(String[] args) {
         int n = 3821;
         int numLength = (int) (Math.log10(n) + 1);
-        System.out.println(reverseNumber2(n, numLength));
+        System.out.println(reverseNumber(n, numLength));
     }
 
-    private static int reverseNumber2(int n, int numLength) {
+    private static int reverseNumber(int n, int numLength) {
         // The second if-block, can save us the last recursive call (which is not
         // needed by the way)
         // if (n <= 0) {
         if (n / 10 <= 0) {
-        // This will check if the number is one digit number
+        // This will check if the number is one-digit number
         // if (n % 10 == n) {
             // At the very last recursive call, let's say the last number is 3, so 3 /
             // 10 is 0, and 0 is less or equal than 0, hence this if block will run,
@@ -20,7 +20,7 @@ public class ReverseNumber {
             return n;
         }
         int remainder = n % 10;
-        return remainder * (int) Math.pow(10, numLength - 1) + reverseNumber2(n / 10,
+        return remainder * (int) Math.pow(10, numLength - 1) + reverseNumber(n / 10,
             numLength - 1);
     }
 }
