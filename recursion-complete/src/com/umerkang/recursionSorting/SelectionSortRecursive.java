138445be43cd257bd1132c07bef7bada096cce4e
Umer Kang <umerkang66@gmail.com>
2022-03-27 16:06:53 +0500

    add comments in selection sort using recursion

--- recursion-complete/src/com/umerkang/recursionSorting/SelectionSortRecursive.java
+++ recursion-complete/src/com/umerkang/recursionSorting/SelectionSortRecursive.java
@@ -14,13 +14,30 @@ public class SelectionSortRecursive {
             return;
         }
         if (b < a) {
+            // Because we passed nums.length as "a" (not nums.length - 1), and we will
+            // iterate (recurse actually) using b, that will always be till the last
+            // index element
             if (nums[b] > nums[max]) {
+                // If "b" (through which we are iterating) is greater than max element
+                // then "b" element becomes max element, and being pass through to the
+                // next function call as max parameter
+                // "b + 1" means move to the next element
                 sort(nums, a, b + 1, b);
             } else {
+                // "b + 1" means move to the next element
+                // "b" element is less than max element, so actual max element should
+                // be passed as the max parameter
                 sort(nums, a, b + 1, max);
             }
         } else {
+            // If "b" is not smaller than "a" (whole length), it means "b" has already
+            // iterated over the array, and found the max element
+            // Swap it with the last possible element, respectively ("a index" is the
+            // length so last possible element is a - 1)
             swap(nums, a - 1, max);
+            // Then again start sorting, but here this should be the next (first for
+            // loop in iterative method), and start the second (for loop) recursive
+            // iteration from zero
             sort(nums, a - 1, 0, 0);
         }
     }
