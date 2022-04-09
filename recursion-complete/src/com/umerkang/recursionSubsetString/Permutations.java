667353190bb2695cab8266662ad807976f9bbf33
umerkang66 <umerkang66@gmail.com>
2022-04-09 20:34:37 +0500

    do questions of Permutations

---
+++ recursion-complete/src/com/umerkang/recursionSubsetString/Permutations.java
@@ -0,0 +1,59 @@
+package com.umerkang.recursionSubsetString;
+
+import java.util.ArrayList;
+
+public class Permutations {
+    public static void main(String[] args) {
+        String str = "abc";
+        permutations("", str);
+        System.out.println(permutationsArray("", str));
+        System.out.println(permutationsCount("", str));
+    }
+
+    private static void permutations(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return;
+        }
+        char ch = unprocessed.charAt(0);
+        for (int i = 0; i <= processed.length(); i++) {
+            String first = processed.substring(0, i);
+            String second = processed.substring(i, processed.length());
+            permutations(first + ch + second, unprocessed.substring(1));
+        }
+    }
+
+    private static ArrayList<String> permutationsArray(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            ArrayList<String> perms = new ArrayList<>();
+            perms.add(processed);
+            return perms;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = unprocessed.charAt(0);
+        // When the processed length increases, number of recursion call increases
+        for (int i = 0; i <= processed.length(); i++) {
+            String first = processed.substring(0, i);
+            String second = processed.substring(i, processed.length());
+            // This will return an arrayList, so add all the elements in new List (ans)
+            ans.addAll(permutationsArray(first + ch + second, unprocessed.substring(1)));
+        }
+        return ans;
+    }
+
+    private static int permutationsCount(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = unprocessed.charAt(0);
+        // When the processed length increases, number of recursion call increases
+        for (int i = 0; i <= processed.length(); i++) {
+            String first = processed.substring(0, i);
+            String second = processed.substring(i, processed.length());
+            // This will return an arrayList, so add all the elements in new List (ans)
+            count += permutationsCount(first + ch + second, unprocessed.substring(1));
+        }
+        return count;
+    }
+}
