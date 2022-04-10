c6caba45b56f2fa71b22363116a894f101cf8c03
umerkang66 <umerkang66@gmail.com>
2022-04-10 20:26:23 +0500

    add and remove some comments

--- recursion-complete/src/com/recursion/recursionBacktracking/AllPaths.java
+++ recursion-complete/src/com/recursion/recursionBacktracking/AllPaths.java
@@ -10,6 +10,9 @@ public class AllPaths {
                 {true, true, true},
                 {true, true, true}
         };
+
+        allPaths("", 0, 0, maze);
+
         int[][] path = new int[maze.length][maze[0].length];
         allPathsPrint("", 0, 0, maze, path, 1);
     }
@@ -45,16 +48,10 @@ public class AllPaths {
         // GO LEFT
         if (col > 0) {
             // To move up, col will remain same, and row will be reduced by one
-            allPaths(p + "U", row, col - 1, maze);
+            allPaths(p + "L", row, col - 1, maze);
         }
 
         // BACKTRACKING
-        // This line is where the function will be over
-        // Before teh function gets removed, also the remove the changes
-        // of the changes that this function has made
-        // After all the recursions in the current cell are being executed
-        // remove the changes that are had been done by every function in this
-        // current recursive cell
         maze[row][col] = true;
     }
 
@@ -62,6 +59,7 @@ public class AllPaths {
         if (row == maze.length - 1 && col == maze[0].length - 1) {
             path[row][col] = step;
             for (int[] arr : path) {
+                // Path for every recursive cell
                 System.out.println(Arrays.toString(arr));
             }
             System.out.println();
@@ -71,10 +69,9 @@ public class AllPaths {
             // Here it is the obstacle [1, 1]
             return;
         }
-
+        // Set the values for this cell only, that can be resetted in the backtracking process
         maze[row][col] = false;
         path[row][col] = step;
-
         // GO DOWN
         if (row < maze.length - 1) {
             // With every function step is increasing
@@ -93,16 +90,9 @@ public class AllPaths {
         // GO LEFT
         if (col > 0) {
             // With every function step is increasing
-            allPathsPrint(p + "U", row, col - 1, maze, path, step + 1);
+            allPathsPrint(p + "L", row, col - 1, maze, path, step + 1);
         }
-
         // BACKTRACKING
-        // This line is where the function will be over
-        // Before teh function gets removed, also the remove the changes
-        // of the changes that this function has made
-        // After all the recursions in the current cell are being executed
-        // remove the changes that are had been done by every function in this
-        // current recursive cell
         maze[row][col] = true;
         path[row][col] = 0;
     }
