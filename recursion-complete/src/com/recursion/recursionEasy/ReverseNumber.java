08d8d6855ca374865337f4705a86e87904e924ca
Umer Kang <umerkang66@gmail.com>
2022-10-10 16:16:29 +0500

    update reverseNumber.java

--- recursion-complete/src/com/recursion/recursionEasy/ReverseNumber.java
+++ recursion-complete/src/com/recursion/recursionEasy/ReverseNumber.java
@@ -3,24 +3,32 @@ package com.recursion.recursionEasy;
 public class ReverseNumber {
     public static void main(String[] args) {
         int n = 3821;
-        int numLength = (int) (Math.log10(n) + 1);
-        System.out.println(reverseNumber(n, numLength));
+        int numDigits = (int) Math.log10(n) + 1;
+
+        System.out.println(reverseNumber(n, numDigits));
+        System.out.println(reverseNumber2(n, 0));
     }
 
-    private static int reverseNumber(int n, int numLength) {
-        // The second if-block, can save us the last recursive call (which is not
-        // needed by the way)
-        // if (n <= 0) {
-        if (n / 10 <= 0) {
-        // This will check if the number is one-digit number
-        // if (n % 10 == n) {
-            // At the very last recursive call, let's say the last number is 3, so 3 /
-            // 10 is 0, and 0 is less or equal than 0, hence this if block will run,
-            // and return that that number
+    private static int reverseNumber(int n, int numDigits) {
+        if (numDigits == 1) {
             return n;
         }
-        int remainder = n % 10;
-        return remainder * (int) Math.pow(10, numLength - 1) + reverseNumber(n / 10,
-            numLength - 1);
+        int lastDigit = n % 10;
+        // add last digit in the beginning, and reverse the other elements
+        // get the first digit to the numDigits place
+        int firstDigitToAdd = lastDigit * (int) Math.pow(10, numDigits - 1);
+        return firstDigitToAdd + reverseNumber(n / 10, numDigits - 1);
+    }
+
+    private static int reverseNumber2(int n, int ans) {
+        if (n == 0) {
+            return ans;
+        }
+        int lastDigit = n % 10;
+        // add the last digit to the ans in the next 10th position,
+        // so multiply the ans with 10, to add the next 10th position,
+        // then add the correct number
+        ans = ans * 10 + lastDigit;
+        return reverseNumber2(n / 10, ans);
     }
 }
--- recursion-complete/src/com/recursion/recursionEasy/SumOfDigits.java
+++ recursion-complete/src/com/recursion/recursionEasy/SumOfDigits.java
@@ -3,13 +3,24 @@ package com.recursion.recursionEasy;
 public class SumOfDigits {
     public static void main(String[] args) {
         int n = 1342;
-        System.out.println(sumOfDigits(n));
+        System.out.println(sumOfDigitsRecursive(n));
+        System.out.println(sumOfDigitsIterative(n));
     }
 
-    private static int sumOfDigits(int n) {
-        if (n == 0) {
+    private static int sumOfDigitsRecursive(int n) {
+        if (n==0) {
             return 0;
         }
-        return n % 10 + sumOfDigits(n / 10);
+        return n % 10 + sumOfDigitsRecursive(n / 10);
+    }
+
+    private static int sumOfDigitsIterative(int n) {
+        int sum = 0;
+        while (n != 0) {
+            // getting the last digit
+            sum += n % 10;
+            n = n / 10;
+        }
+        return sum;
     }
 }
