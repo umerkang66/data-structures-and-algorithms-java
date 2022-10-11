1a062376f78fa98dd057dbcbb1a6b2e56f6c7b09
Umer Kang <umerkang66@gmail.com>
2022-10-11 15:58:47 +0500

    update rotatedBinarySearch

--- recursion-complete/src/com/recursion/recursionArrays/LinearSearch.java
+++ recursion-complete/src/com/recursion/recursionArrays/LinearSearch.java
@@ -7,12 +7,10 @@ public class LinearSearch {
         int[] nums = {3, 2, 1, 18, 18, 9};
         System.out.println(linearSearch(nums, 18, 0));
 
-        linearSearchMultiple(nums, 18, 0);
-        System.out.println(numsAns);
-
         ArrayList<Integer> ansWithArrayList = new ArrayList<>();
-        System.out.println(linearSearchMultipleWithArrayList(nums, 18, 0, ansWithArrayList));
+        System.out.println(linearSearchMultiple(nums, 18, 0, ansWithArrayList));
 
+        System.out.println(linearSearchWithListInBody(nums, 18, 0));
         System.out.println(linearSearchWithListInBody(nums, 18, 0));
     }
 
@@ -28,25 +26,9 @@ public class LinearSearch {
         return linearSearch(nums, target, ++i);
     }
 
-    // With Outside ArrayList
-    static ArrayList<Integer> numsAns = new ArrayList<>();
-
-    private static void linearSearchMultiple(int[] nums, int target, int i) {
-        // We have to check for the last element in array, so when index is greater
-        // than last element, then return -1
-        if (i == nums.length) {
-            return;
-        }
-        if (target == nums[i]) {
-            numsAns.add(i);
-        }
-        linearSearchMultiple(nums, target, ++i);
-    }
-
     // With Inside ArrayList
-    private static ArrayList<Integer> linearSearchMultipleWithArrayList(
-        int[] nums,
-        int target, int i, ArrayList<Integer> ans) {
+    private static ArrayList<Integer> linearSearchMultiple(int[] nums, int target,
+                                                           int i, ArrayList<Integer> ans) {
         // We have to check for the last element in array, so when index is greater
         // than last element, then return -1
         if (i == nums.length) {
@@ -55,25 +37,20 @@ public class LinearSearch {
         if (target == nums[i]) {
             ans.add(i);
         }
-        return linearSearchMultipleWithArrayList(nums, target, ++i, ans);
+        return linearSearchMultiple(nums, target, ++i, ans);
     }
 
     // Create List in Function Body
-    // This is not optimized because objects are creating again and again
-    private static ArrayList<Integer> linearSearchWithListInBody(int[] nums, int target
-        , int i) {
+    private static ArrayList<Integer> linearSearchWithListInBody(int[] nums, int target, int i) {
         if (i == nums.length) {
             // Return this empty list, we will add the answers in when we will
             // return the functions
-            return new ArrayList<Integer>();
+            return new ArrayList<>();
         }
-        // This will contain the answer from current function call only
-        ArrayList<Integer> ansList = new ArrayList<>();
+        ArrayList<Integer> returnedList = linearSearchWithListInBody(nums, target, i + 1);
         if (target == nums[i]) {
-            ansList.add(i);
+            returnedList.add(i);
         }
-        ArrayList<Integer> returnedList = linearSearchWithListInBody(nums, target, ++i);
-        returnedList.addAll(ansList);
         // This should be in opposite direction
         return returnedList;
     }
--- recursion-complete/src/com/recursion/recursionArrays/RotatedBinarySearch.java
+++ recursion-complete/src/com/recursion/recursionArrays/RotatedBinarySearch.java
@@ -2,11 +2,11 @@ package com.recursion.recursionArrays;
 
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[] nums = {5, 6, 7, 8, 9, 1, 2, 3};
-        System.out.println(rotatedBinarySearch(nums, 9, 0, nums.length - 1));
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(nums, 5, 0, nums.length - 1));
     }
 
-    private static int rotatedBinarySearch(int[] nums, int target, int start, int end) {
+    private static int search(int[] nums, int target, int start, int end) {
         if (start > end) {
             return -1;
         }
@@ -14,28 +14,24 @@ public class RotatedBinarySearch {
         if (target == nums[mid]) {
             return mid;
         }
-        // CONDITION # 1
+        // In rotatedArray, two arrays are two ascending sorted arrays are created
         if (nums[start] <= nums[mid]) {
-            // Here we are in the sorted-part of the array
+            // if mid is in the first sorted part of array, then if target is
+            // present between start and mid, apply binary search to only this
+            // part (start to mid)
             if (target >= nums[start] && target <= nums[mid]) {
-                // Check if the target exist in the sorted part of array, if it is,
-                // apply binary search only on this part
-                return rotatedBinarySearch(nums, target, start, mid - 1);
-            } else {
-                // If the target doesn't exist in the sorted part of array, apply
-                // binary search on other (non-sorted) part of the array
-                return rotatedBinarySearch(nums, target, mid + 1, end);
+                return search(nums, target, start, mid);
             }
+            // if mid is present in first sorted part of array, but element is
+            // not present in it, then apply binary search to the remaining
+            // part of first sorted array to the end (including second part of
+            // sorted array) that is from mid + 1, to end
+            return search(nums, target, mid + 1, end);
         }
-        // We are also searching in the second part of array in CONDITION # 1, but if
-        // start element is not smaller than the mid (first part of the array is not
-        // sorted)
-        // Then we have to check in the second part of the array
+        // if mid is in the second sorted part of array, then the same upper login applies here
         if (target >= nums[mid] && target <= nums[end]) {
-            // Here it is certain that this part of array will be sorted
-            return rotatedBinarySearch(nums, target, mid + 1, end);
+            return search(nums, target, mid + 1, end);
         }
-        // If we are searching for the pivot element, then search from start to mid - 1
-        return rotatedBinarySearch(nums, target, start, mid - 1);
+        return search(nums, target, start, mid - 1);
     }
 }
