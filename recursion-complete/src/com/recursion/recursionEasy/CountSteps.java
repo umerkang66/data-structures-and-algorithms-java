7f6482a5641a00e2d6e71ac8f507fa1ccb95a2ec
Umer Kang <umerkang66@gmail.com>
2022-10-11 07:10:22 +0500

    update count steps

--- recursion-complete/src/com/recursion/recursionEasy/CountSteps.java
+++ recursion-complete/src/com/recursion/recursionEasy/CountSteps.java
@@ -8,15 +8,17 @@ public class CountSteps {
         System.out.println(countSteps(n, 0));
     }
 
-    private static int countSteps(int n, int stepNum) {
+    private static int countSteps(int n, int steps) {
         if (n == 0) {
-            return stepNum;
+            return steps;
         }
         // To reduce the number to zero, if the num is divisible by 2, then divide it,
         // otherwise subtract that number with 1
         if (n % 2 == 0) {
-            return countSteps(n / 2, stepNum + 1);
+            // even number
+            return countSteps(n / 2, steps + 1);
         }
-        return countSteps(n - 1, stepNum + 1);
+        // odd number
+        return countSteps(n - 1, steps + 1);
     }
 }
