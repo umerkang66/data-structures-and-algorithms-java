65662ee774a16972b3be58bdcc579fd77c502b9c
Umer Kang <umerkang66@gmail.com>
2022-10-12 14:13:05 +0500

    update patterns with recursion

--- recursion-complete/src/com/recursion/recursionPatterns/Triangle1.java
+++ recursion-complete/src/com/recursion/recursionPatterns/Triangle1.java
@@ -10,27 +10,39 @@ public class Triangle1 {
         if (row == 0) {
             return;
         }
+        // this is like inner loop
         if (col < row) {
             System.out.print("*");
             triangle1(row, col + 1);
-        } else {
-            System.out.println();
-            // when row starts, col starts with zero
-            triangle1(--row, 0);
+            return;
         }
+        // this is like outer loop
+        System.out.println();
+        triangle1(row - 1, 0);
     }
 
     private static void triangle2(int row, int col) {
         if (row == 0) {
             return;
         }
+        // after every call of col increment, col will be equal
+        // to row, thus loop breaks without any printing
         if (col < row) {
             triangle2(row, col + 1);
             System.out.print("*");
-        } else {
-            // when row starts, col starts with zero
-            triangle2(row - 1, 0);
-            System.out.println();
+            return;
         }
+        // after every col increment call runs of current row
+        // will be increased (literally decreased), then in
+        // the second row, again col increment will be called,
+        // and again row increment will be called, on the
+        // process of backtracking first linePrint will be called
+        // then columns should start printing
+        triangle2(row - 1, 0);
+        if (row == 1) {
+            // don't print the first empty line
+            return;
+        }
+        System.out.println();
     }
 }
